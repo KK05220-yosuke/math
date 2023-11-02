c295bf9a7246cf1215d3f673d9864203d5f27795
KK05220-yosuke <144981172+KK05220-yosuke@users.noreply.github.com>
2023-11-02 06:29:25 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,15 +9,15 @@ n = st.slider('次数 (n)', min_value=1, max_value=10, value=2)
 
 # 複素数平面上でx^n=1の解を計算する関数
 def calculate_roots(n):
-    theta = np.linspace(0, 2*np.pi, 1000)  # 0から2πまでの角度を生成
-    roots = np.exp(1j * theta * n)  # 指定された次数nに対する解を計算
+    theta = np.linspace(0, 2*np.pi, n, endpoint=False)  # 0から2πまでの角度を均等にn個生成
+    roots = np.exp(1j * theta)  # 指定された次数nに対する解を計算
     return roots
 
 roots = calculate_roots(n)  # 指定された次数nに対する解を計算
 
 # 複素数平面上に解をプロット
 fig, ax = plt.subplots(figsize=(8, 6))
-ax.plot(roots.real, roots.imag, label=f'x^{n}=1の解')
+ax.plot(roots.real, roots.imag, 'ro', label=f'x^{n}=1の解')
 ax.set_xlabel('実部')
 ax.set_ylabel('虚部')
 ax.axhline(y=0, color='k', linewidth=0.5)  # 虚部が0の軸を描画
@@ -30,4 +30,4 @@ st.pyplot(fig)
 st.header(f'x^{n}=1の解')
 st.table(list(zip(roots.real, roots.imag)))
 
-st.write('注意: 解は複素数平面上に等間隔に配置されています。')
+st.write(f'注意: 解は複素数平面上に均等に{n}個の点として配置されています。')
